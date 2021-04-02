1703cfda38381d57a6ebebe9f7e5210360ade909
Aman Bhimani <amanb014@gmail.com>
2021-04-02 17:24:13 -0400

    story helper

--- src/app/config/story-helper.tsx
+++ src/app/config/story-helper.tsx
@@ -1,8 +1,8 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { networthyTheme } from "./../theme";
 import "@fontsource/open-sans/700.css";
 import "@fontsource/raleway/400.css";
 import React from "react";
+import { networthyTheme } from "./../theme";
 
 export const StoryHelper: React.FC = (props) => (
   <ChakraProvider theme={networthyTheme}>{props.children}</ChakraProvider>
