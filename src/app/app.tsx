437108d38c5123a54c291a67067fcac87e912873
Aman Bhimani <amanb014@gmail.com>
2021-04-02 09:25:03 -0400

    added dependencies and app setup

---
+++ src/app/app.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+
+export const App: React.FC = (_props) => <ChakraProvider></ChakraProvider>;
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { App } from "./app";
 
-ReactDOM.render(<h2>Hello from React!</h2>, document.body);
+ReactDOM.render(<App />, document.body);
